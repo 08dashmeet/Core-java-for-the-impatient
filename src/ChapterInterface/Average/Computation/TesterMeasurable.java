d7c8495d43e516e94906227a67821a4f36ea4f6b
dashmeet <08dskaler@gmail.com>
2018-01-12 17:22:47 +0530

    .

---
+++ src/ChapterInterface/Average/Computation/TesterMeasurable.java
@@ -0,0 +1,9 @@
+package ChapterInterface.Average.Computation;
+
+public class TesterMeasurable {
+    public static void main(String args[]){
+        Employee obj = new Employee(800.56);
+        obj.getMeasure();
+        obj.display();
+    }
+}
