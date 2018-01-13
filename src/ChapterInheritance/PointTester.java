fe95a813b76321c7490ef8336c7dea78da849446
dashmeet <08dskaler@gmail.com>
2018-01-13 20:00:08 +0530

    .

---
+++ src/ChapterInheritance/PointTester.java
@@ -0,0 +1,12 @@
+package ChapterInheritance;
+
+public class PointTester {
+    public static void main(String args[]) {
+        Point obj = new LabeledPoint(90.67, 67.89, "johan");
+
+        LabeledPoint.display();
+    }
+
+
+
+}
