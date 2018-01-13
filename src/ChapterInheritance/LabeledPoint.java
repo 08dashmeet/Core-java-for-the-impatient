269587f33e3285c27b47376cf6ac0c6f2c0e3e29
dashmeet <08dskaler@gmail.com>
2018-01-13 20:00:20 +0530

    .

---
+++ src/ChapterInheritance/LabeledPoint.java
@@ -0,0 +1,20 @@
+package ChapterInheritance;
+
+public class LabeledPoint extends Point {
+   static String label;
+
+    public LabeledPoint(double x,double y,String label){
+        super(x,y);
+        this.label= label;
+
+
+    }
+    public String getLabel(){
+        return label;
+    }
+    public  static void display(){
+        System.out.println("the value of x is "+ x);
+        System.out.println("the value of y is "+ y);
+        System.out.println("the value of Label is "+ label);
+    }
+}
