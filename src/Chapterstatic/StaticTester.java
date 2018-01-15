d6d7ac58e701b6c6c051f61bd7f70419bf68f973
dashmeet <08dskaler@gmail.com>
2018-01-15 13:55:27 +0530

    .

---
+++ src/Chapterstatic/StaticTester.java
@@ -0,0 +1,10 @@
+package Chapterstatic;
+
+public class StaticTester {
+
+    public static void main(String args[]){
+       System.out.println("The radius is"+Student.r+"the circumference is"+Student.Circum());
+
+    }
+
+}
