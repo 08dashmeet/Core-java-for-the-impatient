033304c706b4859e242480a74619296bfe1680a2
dashmeet <08dskaler@gmail.com>
2018-01-15 13:55:15 +0530

    .

---
+++ src/Chapterstatic/Student.java
@@ -0,0 +1,14 @@
+package Chapterstatic;
+
+public class Student {
+
+    static int r = 20;
+
+     public static double  Circum(){
+         double result = 2*3.14*r;
+         return result;
+
+
+
+     }
+}
