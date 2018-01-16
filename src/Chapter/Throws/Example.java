d29b4f65ce474f378b9359d854a67ee00f20766e
dashmeet <08dskaler@gmail.com>
2018-01-16 08:40:43 +0530

    .

---
+++ src/Chapter/Throws/Example.java
@@ -0,0 +1,15 @@
+package Chapter.Throws;
+
+public class Example {
+
+    public void divide(int a, int b) throws ArithmeticException{
+        int c= a/b;
+        System.out.println("the result is "+c);
+    }
+    public void add(int a, int b) {
+        int c= a+b;
+        System.out.println("the result is "+c);
+    }
+
+
+}
