4806600dfa7ad95782cbefe25bd81fd5da29f780
dashmeet <08dskaler@gmail.com>
2018-01-16 08:44:41 +0530

    .

---
+++ src/ChapterThrow.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class ChapterThrow {
+
+    public static void main(String args[]){
+
+        System.out.println("Enter the value of a and b");
+        Scanner sc = new Scanner(System.in);
+         int a=sc.nextInt();
+         int b =sc.nextInt();
+
+        int c= a+b;
+       if(c<50){
+            throw new ArithmeticException();
+        }
+        else {
+            System.out.println("The sum is"+c);
+        }
+
+
+    }
+}
