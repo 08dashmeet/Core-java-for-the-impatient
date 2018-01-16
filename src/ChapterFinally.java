d20a4ffd0991231e30589a1c42c3e773e10d763d
dashmeet <08dskaler@gmail.com>
2018-01-16 07:34:43 +0530

    .

---
+++ src/ChapterFinally.java
@@ -0,0 +1,20 @@
+public class ChapterFinally {
+
+    public static void main(String args[]){
+        try {
+            int[] arr = new int[5];
+            arr[10] = 56;
+            System.out.println(arr[10]);
+        }
+
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBoundException");
+
+        }
+
+        finally{
+
+            System.out.println("finally block");
+        }
+    }
+}
