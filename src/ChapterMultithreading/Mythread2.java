eb8583b68e9034fe92715779c080c926c05d0b67
dashmeet <08dskaler@gmail.com>
2018-01-17 15:42:40 +0530

    .

---
+++ src/ChapterMultithreading/Mythread2.java
@@ -0,0 +1,15 @@
+package ChapterMultithreading;
+
+public class Mythread2 extends Thread {
+    public void run(){
+
+        for(int i=0;i<3;i++)
+        {
+            System.out.println("Thread 2");
+            try{
+                sleep(2000);}
+            catch(Exception e){  }
+        }
+
+    }
+}
