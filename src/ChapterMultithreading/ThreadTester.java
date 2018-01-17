6ad2b15ec7d1e7520512639e4ce46fbd242977d1
dashmeet <08dskaler@gmail.com>
2018-01-17 15:42:51 +0530

    .

---
+++ src/ChapterMultithreading/ThreadTester.java
@@ -0,0 +1,18 @@
+package ChapterMultithreading;
+
+public class ThreadTester {
+
+    public static void main(String args[]) {
+
+        Mythread obj1 = new Mythread();
+        Mythread2 obj2 = new Mythread2();
+
+        obj1.start();
+        try{ obj1.sleep(1000);}
+       catch(Exception e)  { }
+
+        obj2.start();
+
+
+    }
+}
