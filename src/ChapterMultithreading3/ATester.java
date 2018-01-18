d746e1c3f2eee28e6edb53f9e543aec4e312a7c8
dashmeet <08dskaler@gmail.com>
2018-01-18 16:35:24 +0530

    .

--- src/ChapterMultithreading3/ATester.java
+++ src/ChapterMultithreading3/ATester.java
@@ -3,20 +3,33 @@
 package ChapterMultithreading3;
 
 public class ATester {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        A obj1 = new A("One ");
-        A obj2 = new A("Two");
-        A obj3 = new A("Three");
+        A obj1 = new A();
+        B obj2 = new B();
+        Thread t1 = new Thread(obj1);
 
-        try{Thread.sleep(1000);}
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
+
+        try{      t1.join();
+                  t2.join();
+           }
         catch(Exception e){}
 
-        System.out.println("Main method Ending");
+        try{    System.out.println(t1.isAlive());
+                System.out.println(t2.isAlive());
+        }
+        catch(Exception e){}
 
 
-
+        System.out.print("Main method ends here");
 
 
     }
+
 }
+
+
