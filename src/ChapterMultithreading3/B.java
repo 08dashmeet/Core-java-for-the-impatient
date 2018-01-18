06a62a6e0e8cc3b8309236c23c765fcc078f3aa4
dashmeet <08dskaler@gmail.com>
2018-01-18 17:13:09 +0530

    .

--- src/ChapterMultithreading3/B.java
+++ src/ChapterMultithreading3/B.java
@@ -5,9 +5,8 @@ public class B implements Runnable{
     public void run(){
         for(int i=0;i<4;i++){
 
-
-        try{Thread.sleep(1000);}
-        catch(Exception e){}
+            try{Thread.sleep(1000);}
+           catch(Exception e){}
 
         System.out.println("Hello"); }
 
