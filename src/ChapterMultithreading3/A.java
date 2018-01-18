fd5fa5299c9884d74800ecc91e13e33f6196ba72
dashmeet <08dskaler@gmail.com>
2018-01-18 16:35:38 +0530

    .

--- src/ChapterMultithreading3/A.java
+++ src/ChapterMultithreading3/A.java
@@ -1,24 +1,15 @@
 package ChapterMultithreading3;
 
 public class A implements Runnable{
-    String name;
-    Thread t;
-     A(String ename){
-         name = ename;
-         t = new Thread(this,name);
-         System.out.println("New Thread: "+ t);
-         t.start();
 
-     }
 
     public void run(){
+        for(int i=0;i<4;i++){
 
-             System.out.println(name);
+
         try{Thread.sleep(1000);}
         catch(Exception e){}
-
-        System.out.println(name+" Existing");
-
+        System.out.println("Hi");  }
 
     }
 
