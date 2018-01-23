d2df4c0e9d81becd0a68b3aeab87d124efe8c439
dashmeet <08dskaler@gmail.com>
2018-01-23 13:53:15 +0530

    consumer producer example

---
+++ src/ChapterMultithreading/ConsumerProducer/ConsumerProducertester.java
@@ -0,0 +1,54 @@
+package ChapterMultithreading.ConsumerProducer;
+
+public class ConsumerProducertester {
+
+    public static void main(String args[]){
+               ConsumerProducer obj = new ConsumerProducer();
+
+        Thread t1 = new Thread(new Runnable()
+        {
+            public void run(){
+                try{
+                    obj.Producer();
+                }
+                catch(Exception e){}
+            }
+        });
+
+
+        Thread t2 = new Thread(new Runnable()
+        {
+            public void run(){
+                try{
+                    obj.Consumer();
+                }
+                catch(Exception e){}
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+
+        /*try{ t1.join();
+            t2.join();}
+
+            catch(Exception e){}*/
+
+
+
+
+
+      /*  Mythread t1 = new Mythread();
+        Mythread1 t2 = new Mythread1();
+        t1.start();
+        t2.start();
+        try{
+            t1.join();
+            t2.join();
+        }
+        catch (Exception e){}*/
+
+
+    }
+}
